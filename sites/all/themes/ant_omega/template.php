e44ea6b0d9c37af3ec0cc44d89318c485312882b
Anthony Fenwick <anthonyfenwick.1@gmail.com>
2016-10-17 09:13:35 +0100

    Preprocess added to TPL

--- sites/all/themes/ant_omega/template.php
+++ sites/all/themes/ant_omega/template.php
@@ -5,3 +5,10 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Ant Omega theme.
  */
+
+function ant_omega_preprocess_node(&$vars) {
+  //if ($vars['is_front']) {
+    //$vars['title'] = '';
+ // }
+}
+
