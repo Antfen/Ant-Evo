805a26ac15171574284f6981a4cbf643ee94a10d
Anthony Fenwick <anthonyfenwick.1@gmail.com>
2016-11-30 14:38:26 +0000

    Remove preprocess

--- sites/all/themes/ant_omega/template.php
+++ sites/all/themes/ant_omega/template.php
@@ -6,10 +6,4 @@
  * Ant Omega theme.
  */
 
-function yourtheme_preprocess_page(&$vars) {
-  if(drupal_is_front_page()) {
-    drupal_add_js(drupal_get_path('theme', 'ant_omega') . '/js/jquery-2.1.3.min.js');
-  }
-}
-
 
